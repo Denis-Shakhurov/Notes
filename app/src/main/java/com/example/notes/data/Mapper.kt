8232878873cd283ee24de8bdd68f8b14e2dc69e1
DenisShakhurov <disa.sdf86@gmail.com>
2025-11-04 18:01:11 +0700

    #ADD mapper and DTO

---
+++ app/src/main/java/com/example/notes/data/Mapper.kt
@@ -0,0 +1,11 @@
+package com.example.notes.data
+
+import com.example.notes.domain.Note
+
+fun Note.toDBModel(): NoteDBModel {
+    return NoteDBModel(id, title, content, updatedAt, isPinned)
+}
+
+fun NoteDBModel.toEntity(): Note {
+    return Note(id, title, content, updatedAt, isPinned)
+}
