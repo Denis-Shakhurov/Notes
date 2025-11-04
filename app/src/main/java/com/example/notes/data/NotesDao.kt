f18b10706ccceafb21cc652dc08f0e4af1d04e17
DenisShakhurov <disa.sdf86@gmail.com>
2025-11-04 18:21:50 +0700

    #ADD NotesDAO

---
+++ app/src/main/java/com/example/notes/data/NotesDao.kt
@@ -0,0 +1,28 @@
+package com.example.notes.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NotesDao {
+
+    @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
+    fun getAllNotes(): Flow<List<NoteDBModel>>
+
+    @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%' ORDER BY updatedAt DESC")
+    fun searchNotes(query: String): Flow<List<NoteDBModel>>
+
+    @Query("DELETE FROM notes WHERE id == :noteId")
+    suspend fun deleteNote(noteId: Int)
+
+    @Query("UPDATE notes SET isPinned = NOT isPinned WHERE id == :noteId")
+    suspend fun switchPinnedStatus(noteId: Int)
+
+    @Insert(onConflict = REPLACE)
+    suspend fun addNote(noteDBModel: NoteDBModel)
+
+
+}
